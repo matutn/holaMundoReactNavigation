74561027745931b6a1a283e0d44e1bd0a7298b3b
matutn <fausti.matiasgmail.com>
2017-07-02 03:11:55 -0300

    Cambio de Screen hecho

--- index.android.js
+++ index.android.js
@@ -9,18 +9,36 @@ export default class HomeScreen extends React.Component {
   }; 
 
   render() {
-
+    const { navigate } = this.props.navigation;
     return (
       <View>
-          <Text>Hello, Navigation!</Text>
+          <Text>Hello, Chat App!</Text>
+          <Button
+          onPress={() => navigate('Chat')}
+          title="Chat"
+          />
       </View>
       );
   } 
 }
 
+class ChatScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Chat',
+  };
+  render() {
+    return (
+      <View>
+        <Text>Chat</Text>
+      </View>
+    );
+  }
+}
+
+
 const SimpleApp = StackNavigator({
   Home: { screen: HomeScreen },
-/*  Chat: { screen: ChatScreen },*/
+  Chat: { screen: ChatScreen },
 });
 
 AppRegistry.registerComponent('miMundo', () => SimpleApp);
